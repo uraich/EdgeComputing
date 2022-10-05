27ca9d06f272c4162533c4d30f7c69b641cd0cef
uraich <uli.raich@gmail.com>
2022-10-05 16:58:20 +0200

    remove "build" in the flashing script

--- firmware/esp32_flash.py
+++ firmware/esp32_flash.py
@@ -2,4 +2,4 @@
 # flashes the esp32 camera board with MicroPython
 # written by U. Raich for the AIS conference 2020 Kinshasa
 # make sure GPIO-0 is connected to ground through a jumper
-esptool -b 460800 write_flash --flash_size=detect --flash_mode dio 0x1000 build/firmware.bin
+esptool -b 460800 write_flash --flash_size=detect --flash_mode dio 0x1000 firmware.bin
